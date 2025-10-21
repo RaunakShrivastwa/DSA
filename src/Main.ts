484156aacf9e86ffc659a25fa7ad5db51af213af
RaunakShrivastwa <kumarraunak4201@gmail.com>
2025-10-21 12:56:47 +0530

    code changes

--- src/Main.ts
+++ src/Main.ts
@@ -28,7 +28,7 @@ stack.push(20);
 stack.push(30);
 stack.push(40);
 stack.push(50);
-stack.push(60); // This will throw "Stack overflow" error
+// stack.push(60); // This will throw "Stack overflow" error
 
 
 
